0f646da90b79750bafffaf51dc8b0fadefd00c05
Vansh931 <153508058+Vansh931@users.noreply.github.com>
2026-08-06 20:52:28 +0530

    Time: 4 ms (21.15%), Space: 21.1 MB (21.78%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        unordered_set<int>r;
+        unordered_set<int>c;
+        for (int i = 0; i < matrix.size(); i++) {
+            for (int j = 0; j < matrix[i].size(); j++) {
+                if (matrix[i][j] == 0) {
+                    r.insert(i);
+                    c.insert(j);
+                }
+            }
+        }
+        for(int i : r){
+            for(int j = 0;j<matrix[i].size();j++){
+                matrix[i][j]=0;
+            }
+        }
+        for(int i : c){
+            for(int j = 0;j<matrix.size();j++){
+                matrix[j][i]=0;
+            }
+        }
+    }
+
+}
+;

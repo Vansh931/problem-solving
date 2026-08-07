c7344c3db27ce67134612002090a17cce6d5d17b
Vansh931 <153508058+Vansh931@users.noreply.github.com>
2026-08-07 16:45:50 +0530

    Time: 0 ms (100%), Space: 15.7 MB (79.38%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,7 +1,34 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        if (next_permutation(nums.begin(),nums.end())) return;
-        else sort(nums.begin(),nums.end());
+        if(nums.size()==1) return;
+        int n = nums.size();
+        if(nums[n-1]>nums[n-2]){
+            swap(nums[n-1],nums[n-2]);
+            return;
+        }
+        int p = -1;
+        int maxi = n-1;
+        for(int i = n-2;i>=0;i--){
+            if(nums[maxi]<nums[i]) maxi=i;
+            if(nums[i]<nums[i+1]){
+                p=i;
+                break;
+            }
+        }
+        if(p==-1){
+            sort(nums.begin(),nums.end());
+            return;
+        }
+        int s;
+        int mini = 100;
+        for(int i = n-1;i>=p;i--){
+           if(nums[i]>nums[p]&&((mini-nums[p])>(nums[i]-nums[p]))){
+            s=i;
+            mini = nums[i];
+           }
+        }
+        swap(nums[p],nums[s]);
+        sort(nums.begin()+p+1,nums.end());
     }
 };

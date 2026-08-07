623a7ad4f149bb28af12864823d80238de044419
Vansh931 <153508058+Vansh931@users.noreply.github.com>
2026-08-07 15:50:26 +0530

    Time: 0 ms (100%), Space: 15.6 MB (79.38%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        if (next_permutation(nums.begin(),nums.end())) return;
+        else sort(nums.begin(),nums.end());
+    }
+};

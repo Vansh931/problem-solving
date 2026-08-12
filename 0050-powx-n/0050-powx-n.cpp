e4b4ca78ce0ae53ff90fa74c6d988e145dff1f25
Vansh931 <153508058+Vansh931@users.noreply.github.com>
2026-08-12 21:27:41 +0530

    Time: 0 ms (100%), Space: 8.6 MB (71.72%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -2,11 +2,17 @@ class Solution {
 public:
     double findPow(double x, long long n) {
         if(n==0) return 1;
-        else if(n<0) return 1/findPow(x,-n);
         else if(n%2==0) return findPow(x*x,n/2);
         return x*findPow(x,n-1);
     }
     double myPow(double x, int n) {
-        return findPow(x,(long long)n);
+        if(n==0) return 1;
+        else if(n>0) return findPow(x,n);
+        else if(n<0){
+            long long nn = n;
+            nn*=-1;
+            return 1/findPow(x,nn);
+        }
+        return x;
     }
 };

fac981f48d6ecc121b53c35ba194fc3365908ddb
Vansh931 <153508058+Vansh931@users.noreply.github.com>
2026-08-23 20:17:13 +0530

    Time: 0 ms (100%), Space: 17.8 MB (60.77%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -6,15 +6,15 @@ public:
         int prod = INT_MIN;
         int n = nums.size();
         for(int i = 0;i<nums.size();i++){
-            pre*=nums[i];
-            suf*=nums[n-i-1];
-            prod = max(prod,max(pre,suf));
             if(pre==0){
                 pre=1;
             }
             if(suf==0){
                 suf=1;
             }
+            pre*=nums[i];
+            suf*=nums[n-i-1];
+            prod = max(prod,max(pre,suf));
         }
         return prod;
     }
